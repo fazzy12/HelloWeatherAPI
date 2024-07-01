bd7b645becc38bec917f3eb72609bade05d9ffed
fazzy12 <kaluifeanyi00@gmail.com>
2024-07-01 18:11:59 +0100

    push latest update

--- server.js
+++ server.js
@@ -1,15 +1,34 @@
 import express from 'express';
-import routes from './routes/index.js';
-import dotenv from 'dotenv';
-dotenv.config();
-
-
+import axios from 'axios';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
-app.use('/', routes);
+app.get('/api/hello', async (req, res) => {
+  const visitorName = req.query.visitor_name || 'Guest';
+  const clientIp = req.ip || req.connection.remoteAddress;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  try {
+    // Get location and temperature data
+    const geoResponse = await axios.get(`http://ipapi.co/${clientIp}`);
+    const apiKey = process.env.OPENWEATHER_API_KEY
+    const weatherResponse = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${geoResponse.data.city}&appid=${apiKey}&units=metric`);
+
+
+    const location = geoResponse.data.city;
+    const temperature = weatherResponse.data.main.temp;
+
+    res.json({
+      client_ip: clientIp,
+      location: location,
+      greeting: `Hello, ${visitorName}! The temperature is ${temperature} degrees Celsius in ${location}`
+    });
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'An error occurred while processing your request.' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
