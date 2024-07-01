ea7c9bff7054cab72b206ce8001bc0a63635e330
fazzy12 <kaluifeanyi00@gmail.com>
2024-07-01 21:43:44 +0100

    push latest update

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const axios = require('axios');
-const app = express();
 const requestIp = require('request-ip');
-require('dotenv').config();
+const app = express();
 
 const port = process.env.PORT || 3000;
 app.use(requestIp.mw());
@@ -11,7 +10,7 @@ app.get('/api/hello', async (req, res) => {
     const visitorName = req.query.visitor_name;
     let clientIp = req.clientIp || req.ip;
 
-    if ( clientIp === '127.0.0.1' || clientIp === '::1') {
+    if (clientIp === '::1' || clientIp === '127.0.0.1') {
         clientIp = '';
     }
 
@@ -19,9 +18,10 @@ app.get('/api/hello', async (req, res) => {
         let location = 'Unknown';
 
         if (clientIp) {
-            const locationResponse = await axios.get(`https://ipinfo.io/${clientIp}/json?token=5efae1190a428`);
+            const locationResponse = await axios.get(`https://ipinfo.io/${clientIp}/json?token=e6c539ae6f905d`);
             location = locationResponse.data.city || 'Unknown';
         }
+
         const weatherResponse = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=98d479a425d1a70b61341c8f8b615126`);
         const temperature = weatherResponse.data.main.temp;
 
@@ -30,6 +30,8 @@ app.get('/api/hello', async (req, res) => {
             location: location,
             greeting: `Hello, ${visitorName}! The temperature is ${temperature} degrees Celsius in ${location}.`
         });
+
+        console.log('Request processed successfully.');
     } catch (error) {
         console.error('Error:', error.message);
 
@@ -38,10 +40,11 @@ app.get('/api/hello', async (req, res) => {
             console.error('Error Response Status:', error.response.status);
         }
 
-        res.status(500).json({ error: 'error occurred' });
+        res.status(500).json({ error: 'An error occurred' });
     }
 });
 
 app.listen(port, () => {
-    console.log(`Server running at ${port}`);
+    console.log(`Server running at http://localhost:${port}`);
+    console.log('Server started successfully.');
 });
